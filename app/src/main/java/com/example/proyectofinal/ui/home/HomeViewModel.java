a259b33e7e0b844ced64b6c0cf3760caf6ea30fa
gcastillo15 <gcastillo1510s@gmail.com>
2021-12-07 20:45:55 -0500

    Inserción de imagen del último fragment

--- app/src/main/java/com/example/proyectofinal/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/proyectofinal/ui/home/HomeViewModel.java
@@ -10,7 +10,17 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Universidad Tecnológica de Panamá \n"  +
+                "Factultad de Ingeniería de Sistemas Computacionales\n" +
+                "Lic. en Ingeniería de Sistemas y Computación\n\n" +
+                "Proyecto Semestral - Sensores\n\n" +
+                "Integrantes: \n" +
+                "Aizprúa, Pamela \n" +
+                "Castillo, Gilberto \n" +
+                "Tack, Mark \n\n" +
+                "Profesora: \n" +
+                "Marlina Sanchez\n\n" +
+                "II Semestre-2021\n");
     }
 
     public LiveData<String> getText() {
